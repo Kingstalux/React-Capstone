2907748034791cc2b995661d90b7314c19bbc9b6
Kingstalux <kingsleyngu25@gmail.com>
2021-12-03 11:44:25 +0100

    Add tests

---
+++ src/_tests_/Details.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import Details from '../components/Details';
+
+describe('Details Element Tests', () => {
+  test('Check if Component Renders in DOM', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+            <Details />
+        </Router>
+      </Provider>,
+    );
+    expect(screen.queryByText(/2021/)).toBeNull();
+    expect(screen.queryByText(/Description/)).toBeNull();
+  });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+        <Router>
+            <Details />
+        </Router>
+      </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
