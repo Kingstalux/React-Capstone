4288b9a0dfaabd732d95f542880ab2d1bd9e5e66
Kingstalux <kingsleyngu25@gmail.com>
2021-12-03 06:38:51 +0100

    Add styles for details desktop page

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,42 +5,75 @@ export default function Details() {
   const countryStats = useSelector((state) => state.detailsReducer.data);
 
   const covidStats = countryStats.map((element) => (
-    <div key={element}>
-      <img src={element.flag} alt="flag" />
-      <p>
-        Country:
-        {element.name}
-      </p>
-      <p>
-        Active Cases:
-        {element.cases}
-      </p>
-      <p>
-        Deaths:
-        {element.deaths}
-      </p>
-      <p>
-        Recovered:
-        {element.recovered}
-      </p>
-      <p>
-        Today&apos;s Cases:
-        {element.todayCases}
-      </p>
-      <p>
-        Today&apos;s Deaths:
-        {element.todayDeaths}
-      </p>
-      <p>
-        Recovered Today:
-        {element.todayRecovered}
-      </p>
+    <div key={element} className="country-stats">
+      <div className="flag">
+        <img src={element.flag} alt="flag" />
+      </div>
+      <h2>Stats Breakdown - 2021</h2>
+      <div className="stats-card">
+        <div className="stats">
+          <h3>
+            Country:
+          </h3>
+          <p>
+            {element.name}
+          </p>
+        </div>
+        <div className="stats">
+          {' '}
+          <h3>
+            Active Cases:
+          </h3>
+          <p>
+            {element.cases}
+          </p>
+        </div>
+        <div className="stats">
+          <h3>
+            Deaths:
+          </h3>
+          <p>
+            {element.deaths}
+          </p>
+        </div>
+        <div className="stats">
+          <h3>
+            Recovered:
+          </h3>
+          <p>
+            {element.recovered}
+          </p>
+        </div>
+        <div className="stats">
+          <h3>
+            Today&apos;s Cases:
+          </h3>
+          <p>
+            {element.todayCases}
+          </p>
+        </div>
+        <div className="stats">
+          <h3>
+            Today&apos;s Deaths:
+          </h3>
+          <p>
+            {element.todayDeaths}
+          </p>
+        </div>
+        <div className="stats">
+          <h3>
+            Recovered Today:
+          </h3>
+          <p>
+            {element.todayRecovered}
+          </p>
+        </div>
+      </div>
     </div>
   ));
 
   return (
     <div>
-      <h2>Details</h2>
       {covidStats}
     </div>
   );
